b12764b6a5fcdcf1a8400fbb934734582e79fed1
Styrse <styrbjoern@me.com>
2025-03-31 13:35:31 +0200

    Update

--- src/main/java/app/handler/RouteHandler.java
+++ src/main/java/app/handler/RouteHandler.java
@@ -1,11 +1,16 @@
 package app.handler;
 
+import app.entities.BasketItem;
+import app.entities.itemTypes.Item;
+import app.entities.itemTypes.eatables.Cupcake;
 import app.entities.itemTypes.eatables.CupcakeBottom;
 import app.entities.itemTypes.eatables.CupcakeTop;
 import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
 import io.javalin.Javalin;
+import io.javalin.http.Context;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,9 +23,10 @@ public class RouteHandler {
     public static void routes(Javalin app, ConnectionPool connectionPool){
         app.get("/", ctx -> showCupcakes(ctx));
 
-        app.get("/basket", ctx -> {
+        app.get("/basket", ctx -> showBasket(ctx));
+        /*app.get("/basket", ctx -> {
             ctx.render("basket.html");
-        });
+        });*/
 
         app.get("/index", ctx -> {
             ctx.render("index.html");
@@ -44,6 +50,25 @@ public class RouteHandler {
             ctx.render("index.html", model);
         } catch (DatabaseException e) {
             ctx.status(500).result("Database error: " + e.getMessage());
+        }
+    }
+
+    public static void showBasket(Context ctx) {
+        try {
+            List<BasketItem> basket = new ArrayList<>();
+            //TODO: Add get List method
+            basket.add(new BasketItem(3, new Cupcake(getCupcakeBottoms(connectionPool).get(2), getCupcakeTops(connectionPool).get(1))));
+            basket.add(new BasketItem(1, new Cupcake(getCupcakeBottoms(connectionPool).get(1), getCupcakeTops(connectionPool).get(3))));
+            basket.add(new BasketItem(2, new Cupcake(getCupcakeBottoms(connectionPool).get(3), getCupcakeTops(connectionPool).get(3))));
+            basket.add(new BasketItem(4, new Cupcake(getCupcakeBottoms(connectionPool).get(3), getCupcakeTops(connectionPool).get(1))));
+            basket.add(new BasketItem(1, new Cupcake(getCupcakeBottoms(connectionPool).get(4), getCupcakeTops(connectionPool).get(1))));
+
+            Map<String, Object> model = new HashMap<>();
+            model.put("basket", basket);
+
+            ctx.render("basket.html", model);
+        } catch (DatabaseException e) {
+            throw new RuntimeException(e);
         }
     }
 }
