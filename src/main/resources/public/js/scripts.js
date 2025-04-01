5fa5e95c098ec6dc9b667c36bd990129f50c5a1a
Styrse <styrbjoern@me.com>
2025-04-01 18:32:40 +0200

    Restored js

--- src/main/resources/public/js/scripts.js
+++ src/main/resources/public/js/scripts.js
@@ -55,3 +55,17 @@ document.addEventListener("DOMContentLoaded", function () {
     // Initial total price update
     updateTotalPrice();
 });
+
+document.querySelectorAll(".menu").forEach(menu => {
+    menu.addEventListener("click", function() {
+        let img = menu.querySelector("img");
+        if (img && img.hasAttribute("href")) {
+            let targetElement = document.getElementById(img.getAttribute("href"));
+
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: "smooth" });
+            }
+        }
+    });
+});
+
