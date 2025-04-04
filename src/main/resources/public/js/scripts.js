281fa06ba1b93ee1e5b78afad7b87038ca496bd2
Styrse <styrbjoern@me.com>
2025-04-04 11:05:23 +0200

    cupcakes

--- src/main/resources/public/js/scripts.js
+++ src/main/resources/public/js/scripts.js
@@ -57,15 +57,44 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 document.querySelectorAll(".menu").forEach(menu => {
-    menu.addEventListener("click", function() {
+    menu.addEventListener("click", function () {
         let img = menu.querySelector("img");
         if (img && img.hasAttribute("href")) {
             let targetElement = document.getElementById(img.getAttribute("href"));
 
             if (targetElement) {
-                targetElement.scrollIntoView({ behavior: "smooth" });
+                targetElement.scrollIntoView({behavior: "smooth"});
             }
         }
     });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    let bottomId = null;
+    let topId = null;
+
+    const previewImg = document.getElementById('cupcake-preview-img');
+
+    document.querySelectorAll('.cupcakeBottomRadio').forEach(input => {
+        input.addEventListener('change', (e) => {
+            bottomId = parseInt(e.target.value);
+            updateImage();
+        });
+    });
+
+    document.querySelectorAll('.cupcakeTopRadio').forEach(input => {
+        input.addEventListener('change', (e) => {
+            topId = parseInt(e.target.value);
+            updateImage();
+        });
+    });
+
+    function updateImage() {
+        if (bottomId !== null && topId !== null) {
+            const imageNumber = (bottomId - 1) * 9 + topId;
+            previewImg.src = `/img/icons/cupcakes/${imageNumber}.png`;
+        }
+    }
+});
+
 
